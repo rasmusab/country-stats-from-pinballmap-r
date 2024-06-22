d45db5843e74d41477a76af93df77b06863a0535
Rasmus Bååth <rasmus.baath@gmail.com>
2024-06-22 18:36:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Country-level Pinball Location Statistics Over Time
+
+This repo is meant as a minimal example of the [git scraping](https://simonwillison.net/2020/Oct/9/git-scraping/) pattern using R. It uses [the API of pinballmap.com](https://pinballmap.com/api/v1/docs/1.0/locations/index.html) to pull down the latest country-level stats on how many public pinball locations each country has, once a week.
+This data is also persisted and versioned in this very repository.
+
+![](top-10-countries.svg)
---
+++ get-countries-json.R
@@ -0,0 +1,68 @@
+# This script fetches the latest country statistics from pinballmap.com, 
+# saves the data in JSON format, and tracks historical changes in a CSV file.
+
+library(tidyverse)
+library(httr2)
+library(jsonlite)
+
+# Fetch the latest country statistics from pinballmap.com
+response <- req_perform(request("https://pinballmap.com/api/v1/locations/countries.json"))
+
+if (resp_status(response) != 200) {
+  stop("Failed to retrieve data: ", resp_status(response))
+}
+
+countries_json <- resp_body_json(response)
+
+# Write the JSON data to a file in pretty print format
+# Why pretty print? Because it diffs better in git.
+write_json(countries_json, "countries.json", pretty = TRUE, auto_unbox = TRUE)
+
+# And stick it in the history folder with a timestamp, as well.
+dir.create("json-history", showWarnings = FALSE)
+history_file_path <- paste0("json-history/", format(Sys.time(), "%Y-%m-%d_"), "countries.json")
+file.copy("countries.json", history_file_path)
+
+# Read all historical JSON files and prepare them for analysis
+json_fnames <- list.files("json-history", pattern = "*.json", full.names = TRUE)
+countries_history <- 
+  map(json_fnames, function(fname) {
+    fromJSON(fname) |>
+      select(country, location_count) |>
+      mutate(date = as.Date(str_extract(fname, "\\d{4}-\\d{2}-\\d{2}")))
+  }) |> 
+  bind_rows() |>
+  arrange(date, country)
+
+# Save the countries' history to a CSV file
+write_csv(countries_history, "countries-history.csv")
+
+# That concludes the neccecary steps to fetch and store the pinballmap's data
+# buuuuut, let's also plot the timeseries for the top 10 current contries,
+# just for the fun of it :) 
+
+latest_date <- max(countries_history$date)
+
+top_10_countries <- countries_history |>
+  filter(date == latest_date) |>
+  slice_max(location_count, n = 10) |> 
+  pull(country)
+
+top_10_countries_history <- countries_history |>
+  filter(country %in% top_10_countries) |>
+  mutate(country = fct_relevel(country, top_10_countries))
+
+# Plot the top 10 countries' history
+top_10_countries_plot <- ggplot(top_10_countries_history, aes(x = date, y = location_count, color = country)) +
+  geom_line() +
+  geom_point() +
+  scale_x_date(date_breaks = "1 month", date_labels = "%Y %b") +
+  scale_y_log10(labels = scales::comma) +
+  labs(
+    title = "Top 10 countries with most public pinball locations",
+    subtitle = paste0("as of ", latest_date),
+    x = "",
+    y = "Number of locations"
+  )
+
+ggsave("top-10-countries.svg", top_10_countries_plot, width = 7, height = 4)
